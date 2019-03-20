827dadb9d4d49835f047144ed5c67a76ca5e9f6c
Etienne Blanc-Coquand <etienne1296pro@gmail.com>
2019-03-20 09:31:57 +0100

    Update

--- index.php
+++ index.php
@@ -3,10 +3,17 @@
   $data = json_decode($data, true);
   $url = '/create';
   echo '<a href="create.php">Créer</a>';
+  ?> <div id="parent"><?php
   foreach ($data as $item) {
     echo '<h1>Id :' . $item['index'] . '</h1>';
     echo '<h2>' . $item['name']['first'] . ' ' . $item['name']['last'] . '</h2>';
     echo '<h3>Company : ' . $item['company'] . ' </h3>';
-    echo '<button onClick="">Supprimer</button>';
+    echo '<button class="d">Supprimer</button>';
     echo '<hr>';
   }
+?>
+</div>
+
+<script>
+  document.getElementById('parent').getElementsByClassName('d')[0].onclick = () => { alert('Y a rien pour le moment'); }
+</script>
